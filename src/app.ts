11a9df252d2344ce4290a6b54cd099e38755b749
Thiago Adomaitis <t.d.adomaitis@gmail.com>
2021-07-15 22:14:09 -0300

    add code to start the server

--- src/app.ts
+++ src/app.ts
@@ -47,3 +47,12 @@ const runningMessage = `Server running at http://localhost:${port}`
 app.get('/', (req: express.Request, res: express.Response) => {
   res.status(200).send(runningMessage)
 })
+
+server.listen(port, () => {
+  routes.forEach((route: CommonRoutesConfig) => {
+    debugLog(`Routes configured for ${route.getName()}`)
+  })
+  // our only exception to avoiding console.log(), because we
+  // always want to know when the server is done starting up
+  console.log(runningMessage)
+})
