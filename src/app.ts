22aa652b2b08e55f027071813ea336aebef7fcbc
Thiago Adomaitis <t.d.adomaitis@gmail.com>
2021-07-15 22:09:04 -0300

    set some middlewares

--- src/app.ts
+++ src/app.ts
@@ -13,3 +13,9 @@ const server: http.Server = http.createServer(app)
 const port = 3000
 const routes: Array<CommonRoutesConfig> = []
 const debugLog: debug.IDebugger = debug('app')
+
+// middleware to parse all incoming requests as JSON
+app.use(express.json())
+
+// middleware to allow cross-origin requests
+app.use(cors())
