4e06cb079a066ad1461ea6e05cb5f016fb3cf11c
Thiago Adomaitis <t.d.adomaitis@gmail.com>
2021-07-15 22:08:36 -0300

    Import things and create consts

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express from 'express'
+import * as http from 'http'
+
+import * as winston from 'winston'
+import * as expressWinston from 'express-winston'
+import cors from 'cors'
+import { CommonRoutesConfig } from './common/common.routes.config'
+import { UsersRoutes } from './users/users.routes.config'
+import debug from 'debug'
+
+const app: express.Application = express()
+const server: http.Server = http.createServer(app)
+const port = 3000
+const routes: Array<CommonRoutesConfig> = []
+const debugLog: debug.IDebugger = debug('app')
