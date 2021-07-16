5efd2a98f4b0f833c799659353e78ae3711d6e8c
Thiago Adomaitis <t.d.adomaitis@gmail.com>
2021-07-15 22:13:15 -0300

    add the UserRoutes to routes array

--- src/app.ts
+++ src/app.ts
@@ -37,3 +37,7 @@ if (!process.env.DEBUG) {
 
 // initialize the logger with the configuration above
 app.use(expressWinston.logger(loggerOptions))
+
+// here we are adding the UserRoutes to our array,
+// after sending the Express.js application object to have the routes added to our app!
+routes.push(new UsersRoutes(app))
