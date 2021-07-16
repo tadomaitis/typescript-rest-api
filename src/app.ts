46d7a60f836223570fdbad5ea194f86a8d12573e
Thiago Adomaitis <t.d.adomaitis@gmail.com>
2021-07-15 22:13:40 -0300

    test route, to check if it is working

--- src/app.ts
+++ src/app.ts
@@ -41,3 +41,9 @@ app.use(expressWinston.logger(loggerOptions))
 // here we are adding the UserRoutes to our array,
 // after sending the Express.js application object to have the routes added to our app!
 routes.push(new UsersRoutes(app))
+
+// this is a simple route to make sure everything is working properly
+const runningMessage = `Server running at http://localhost:${port}`
+app.get('/', (req: express.Request, res: express.Response) => {
+  res.status(200).send(runningMessage)
+})
