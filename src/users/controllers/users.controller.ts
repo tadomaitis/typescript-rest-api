f0bef46ef749815c303b3d22a16af0f09b63b455
Thiago Adomaitis <t.d.adomaitis@gmail.com>
2021-07-17 21:56:57 -0300

    add users controller

---
+++ src/users/controllers/users.controller.ts
@@ -0,0 +1,38 @@
+import { Request, Response } from 'express'
+import usersService from '../services/users.service'
+import argon2 from 'argon2'
+import debug from 'debug'
+
+const log: debug.IDebugger = debug('app:users-controller')
+class UsersController {
+  async listUsers (req: Request, res: Response) {
+    const users = await usersService.list(100, 0)
+    res.status(200).send(users)
+  }
+
+  async getUserById (req: Request, res: Response) {
+    const user = await usersService.readById(req.body.id)
+    res.status(200).send(user)
+  }
+
+  async patch (req: Request, res: Response) {
+    if (req.body.password) {
+      req.body.password = await argon2.hash(req.body.password)
+    }
+    log(await usersService.patchById(req.body.id, req.body))
+    res.status(204).send()
+  }
+
+  async put (req: Request, res: Response) {
+    req.body.password = await argon2.hash(req.body.password)
+    log(await usersService.putById(req.body.id, req.body))
+    res.status(204).send()
+  }
+
+  async removeUser (req: Request, res: Response) {
+    log(await usersService.deleteById(req.body.id))
+    res.status(204).send()
+  }
+}
+
+export default new UsersController()
