4ef8dde9a17828d5e34f59e8d2ad0371817d4701
Thiago Adomaitis <t.d.adomaitis@gmail.com>
2021-07-19 08:42:12 -0300

    refactor to use UsersController and UsersMiddleware

--- src/users/users.routes.config.ts
+++ src/users/users.routes.config.ts
@@ -1,47 +1,41 @@
 import { CommonRoutesConfig } from '../common/common.routes.config'
-import express from 'express'
+import UsersController from './controllers/users.controller'
+import UsersMiddleware from './middleware/users.middleware'
+import { Application } from 'express'
 
 export class UsersRoutes extends CommonRoutesConfig {
-  constructor (app: express.Application) {
+  constructor (app: Application) {
     super(app, 'UserRoutes')
   }
 
-  configureRoutes () {
+  configureRoutes (): Application {
     this.app
       .route('/users')
-      .get((req: express.Request, res: express.Response) => {
-        res.status(200).send('List of users')
-      })
-      .post((req: express.Request, res: express.Response) => {
-        res.status(200).send('Post to users')
-      })
+      .get(UsersController.listUsers)
+      .post(
+        UsersMiddleware.validateRequiredUserBodyFields,
+        UsersMiddleware.validateSameEmailDoesntExist,
+        UsersController.createUser
+      )
+
+    this.app.param('userId', UsersMiddleware.extractUserId)
 
     this.app
       .route('/users/:userId')
-      .all(
-        (
-          req: express.Request,
-          res: express.Response,
-          next: express.NextFunction
-        ) => {
-          // this middleware function runs before any request to /users/:userId
-          // but it doesn't accomplish anything just yet
-          // it simply pass control to the next applicable function below using next()
-          next()
-        }
-      )
-      .get((req: express.Request, res: express.Response) => {
-        res.status(200).send(`GET requested for id ${req.params.userId}`)
-      })
-      .put((req: express.Request, res: express.Response) => {
-        res.status(200).send(`PUT requested for id ${req.params.userId}`)
-      })
-      .patch((req: express.Request, res: express.Response) => {
-        res.status(200).send(`PATCH requested for id ${req.params.userId}`)
-      })
-      .delete((req: express.Request, res: express.Response) => {
-        res.status(200).send(`DELETE requested for id ${req.params.userId}`)
-      })
+      .all(UsersMiddleware.validadeUserExists)
+      .get(UsersController.getUserById)
+      .delete(UsersController.removeUser)
+
+    this.app.put('/users/:userId', [
+      UsersMiddleware.validateRequiredUserBodyFields,
+      UsersMiddleware.validateSameEmailBelongToSameUser,
+      UsersController.put
+    ])
+
+    this.app.patch('/users/:userId', [
+      UsersMiddleware.validatePatchEmail,
+      UsersController.patch
+    ])
 
     return this.app
   }
