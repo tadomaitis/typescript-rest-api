e148f6a0bd0486b2cf3f8cdb224babd861b84b3a
Thiago Adomaitis <t.d.adomaitis@gmail.com>
2021-07-22 10:25:11 -0300

    create body validation middleware

---
+++ src/common/middlware/body.validation.middleware.ts
@@ -0,0 +1,14 @@
+import { Request, Response, NextFunction } from 'express'
+import { validationResult } from 'express-validator'
+
+class BodyValidationMiddleware {
+  verifyBodyFielsErrors (req: Request, res: Response, next: NextFunction) {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).send({ errors: errors.array() })
+    }
+    next()
+  }
+}
+
+export default new BodyValidationMiddleware()
