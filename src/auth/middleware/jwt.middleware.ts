0577df0fd8cf7b07c0482b42859b563c7f87db15
Thiago Adomaitis <t.d.adomaitis@gmail.com>
2021-08-02 11:01:23 -0300

    create function to  verify the presence of a refresh token

--- src/auth/middleware/jwt.middleware.ts
+++ src/auth/middleware/jwt.middleware.ts
@@ -1,5 +1,20 @@
+import { Request, Response, NextFunction } from 'express'
+
 class JwtMiddleware {
-  
+  verifyRefreshBodyField(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) {
+    if (req.body && req.body.refreshToken) {
+      return next()
+    } else {
+      return res
+            .status(400)
+            .send({ errors: ['Missing required field: refreshToken'] })
+    }
+  }
+
 }
 
 export default new JwtMiddleware()
