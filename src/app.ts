870d4ed77c7b3eb3581818e9821d481063d679a1
Thiago Adomaitis <t.d.adomaitis@gmail.com>
2021-08-02 10:23:54 -0300

    add auth routes to mainfile

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,7 @@ import * as expressWinston from 'express-winston'
 import cors from 'cors'
 import { CommonRoutesConfig } from './common/common.routes.config'
 import { UsersRoutes } from './users/users.routes.config'
+import { AuthRoutes } from './auth/auth.routes.config'
 import debug from 'debug'
 
 const app: express.Application = express()
@@ -45,6 +46,7 @@ app.use(expressWinston.logger(loggerOptions))
 // here we are adding the UserRoutes to our array,
 // after sending the Express.js application object to have the routes added to our app!
 routes.push(new UsersRoutes(app))
+routes.push(new AuthRoutes(app))
 
 // this is a simple route to make sure everything is working properly
 const runningMessage = `Server running at http://localhost:${port}`
