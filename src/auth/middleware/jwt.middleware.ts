059f025ad5b34ef8ab684ee57be2a80cc7c7aeab
Thiago Adomaitis <t.d.adomaitis@gmail.com>
2021-08-02 11:02:42 -0300

    create function to verify the refresh token

--- src/auth/middleware/jwt.middleware.ts
+++ src/auth/middleware/jwt.middleware.ts
@@ -1,4 +1,9 @@
 import { Request, Response, NextFunction } from 'express'
+import crypto from 'crypto'
+import usersService from '../../users/services/users.service'
+
+// @ts-expect-error
+const jwtSecret: string = process.env.JWT_SECRET
 
 class JwtMiddleware {
   verifyRefreshBodyField(
@@ -12,6 +17,28 @@ class JwtMiddleware {
       return res
             .status(400)
             .send({ errors: ['Missing required field: refreshToken'] })
+    }
+  }
+
+  async validRefreshNeeded(req: Request, res: Response, next: NextFunction) {
+    const user: any = await usersService.getUserByEmailWithPassword(
+      res.locals.jwt.email
+    )
+    const salt = crypto.createSecretKey(
+      Buffer.from(res.locals.jwt.refreshKey.data)
+    )
+    const hash = crypto
+      .createHmac('sha512', salt)
+      .update(res.locals.jwt.userId + jwtSecret).digest('base64')
+    if (hash === req.body.refreshToken) {
+      req.body = {
+        userId: user._id,
+        email: user.email,
+        permissionFlags: user.permissionFlags
+      }
+      return next()
+    } else {
+      return res.status(400).send({ errors: ['Invalid refreshToken']})
     }
   }
 
