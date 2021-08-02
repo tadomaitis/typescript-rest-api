25321fb5605d769aa938910af2d25b7c4925e0cc
Thiago Adomaitis <t.d.adomaitis@gmail.com>
2021-08-02 11:12:00 -0300

    import jwtMiddleware to auth routes file

--- src/auth/auth.routes.config.ts
+++ src/auth/auth.routes.config.ts
@@ -1,6 +1,7 @@
 import {CommonRoutesConfig} from '../common/common.routes.config'
 import authController from './controller/auth.controller'
 import authMiddleware from './middleware/auth.middleware'
+import jwtMiddleware from './middleware/jwt.middleware'
 import { Application } from 'express'
 import BodyValidationMiddleware from '../common/middlware/body.validation.middleware'
 import { body } from 'express-validator'
@@ -16,9 +17,13 @@ export class AuthRoutes extends CommonRoutesConfig {
       body('password').isString(),
       BodyValidationMiddleware.verifyBodyFielsErrors,
       authMiddleware.verifyUserPassword,
+      authController.createJWT      
+    ])
+    this.app.post('/auth/refresh-token', [
+      jwtMiddleware.validJWTNeeded,
+      jwtMiddleware.verifyRefreshBodyField,
+      jwtMiddleware.validRefreshNeeded,
       authController.createJWT
-
-      
     ])
 
     return this.app
